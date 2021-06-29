cf1e8d647494ea753b1be91c95e8a13d8d8d9966
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-29 13:51:38 +0530

    [Shreya] Add:  Welcome to Indian States Census Analyser Problem

---
+++ IndianStatesCensusAnalyserProblem/IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser Problem");
+        }
+    }
+}
